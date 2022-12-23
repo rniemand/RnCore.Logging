4fa56a35c4d41b132ac0570f28751c38db2d8c27
Richard Niemand <niemand.richard@gmail.com>
2022-12-22 17:15:36 -0700

    Adding SonarQube support

---
+++ tests/RnCore.LoggingTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RnCore.LoggingTests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+  }
+}
